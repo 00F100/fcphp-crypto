28553bed1d224f662261c8e131d5652595d84518
Joao Moraes <joaomoraesbr@gmail.com>
2018-06-16 22:24:01 -0300

    add comments and interface

--- src/Crypto.php
+++ src/Crypto.php
@@ -6,14 +6,30 @@ namespace FcPhp\Crypto
 
 	class Crypto implements ICrypto
 	{
+		/**
+		 * @var string
+		 */
 		private $nonce;
 
+		/**
+		 * Method to construct instance of Crypto
+		 *
+		 * @param string $nonce Nonce to crypt string
+		 * @return void
+		 */
 		public function __construct(?string $nonce = null)
 		{
 			$this->nonce = !empty($nonce) ? base64_decode($nonce) : $this->generateNonce();
 		}
 
-		public function encode(string $key, $input)
+		/**
+		 * Method to encode string
+		 *
+		 * @param string $key Key to encode
+		 * @param mixed $input Input to crypt
+		 * @return string
+		 */
+		public function encode(string $key, $input) :string
 		{
 			$keyDecode = base64_decode($key);
 			$keypair1_public = $this->getPublic($keyDecode);
@@ -21,6 +37,13 @@ namespace FcPhp\Crypto
 			return $this->_encode(serialize($input), $keypair1_public, $keypair1_secret);
 		}
 
+		/**
+		 * Method to decode string
+		 *
+		 * @param string $key Key to decode
+		 * @param string $input String to decode
+		 * @return mixed
+		 */
 		public function decode(string $key, string $input)
 		{
 			$keyDecode = base64_decode($key);
@@ -29,38 +52,82 @@ namespace FcPhp\Crypto
 			return unserialize($this->_decode($input, $keypair1_public, $keypair1_secret));
 		}
 
-		public function generateKey()
+		/**
+		 * Method to generate new key
+		 *
+		 * @return string
+		 */
+		public function generateKey() :string
 		{
 			return base64_encode(sodium_crypto_box_keypair());
 		}
 
-		public function generateNonce()
+		/**
+		 * Method to generate new nonce
+		 *
+		 * @return string
+		 */
+
+		public function generateNonce() :string
 		{
 			return random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES);
 		}
 
-		public static function getNonce()
+		/**
+		 * Method to generate new nonce
+		 *
+		 * @return string
+		 */
+		public static function getNonce() :string
 		{
 			return base64_encode(random_bytes(SODIUM_CRYPTO_BOX_NONCEBYTES));
 		}
 
+		/**
+		 * Method to decode string
+		 *
+		 * @param string $input Input to decode
+		 * @param string $keyPublic Key public
+		 * @param string $keySecret Key secret
+		 * @return string
+		 */
 		private function _decode(string $input, string $keyPublic, string $keySecret)
 		{
 			$decryption_key = sodium_crypto_box_keypair_from_secretkey_and_publickey(base64_decode($keySecret), base64_decode($keyPublic));
 			return sodium_crypto_box_open(base64_decode($input), $this->nonce, $decryption_key);
 		}
 
+		/**
+		 * Method to encode string
+		 *
+		 * @param string $input Input to encode
+		 * @param string $keyPublic Key public
+		 * @param string $keySecret Key secret
+		 * @return string
+		 */
 		private function _encode(string $input, string $keyPublic, string $keySecret)
 		{
 			$encryption_key = sodium_crypto_box_keypair_from_secretkey_and_publickey(base64_decode($keySecret), base64_decode($keyPublic));
 			return base64_encode(sodium_crypto_box($input, $this->nonce, $encryption_key));
 		}
 
+		/**
+		 * Method to return secret of key
+		 *
+		 * @param string $key
+		 * @return string
+		 */
 		private function getSecret(string $key)
 		{
 			return base64_encode(sodium_crypto_box_secretkey($key));
 		}
 
+		/**
+		 * Method to return public of key
+		 *
+		 * @param string $key
+		 * @return string
+		 */
 		private function getPublic(string $key)
 		{
 			return base64_encode(sodium_crypto_box_publickey($key));
--- src/Interfaces/ICrypto.php
+++ src/Interfaces/ICrypto.php
@@ -4,6 +4,16 @@ namespace FcPhp\Crypto\Interfaces
 {
 	interface ICrypto
 	{
+		public function __construct(?string $nonce = null);
 
+		public function encode(string $key, $input) :string;
+
+		public function decode(string $key, string $input);
+
+		public function generateKey() :string;
+
+		public function generateNonce() :string;
+
+		public static function getNonce() :string;
 	}
 }
